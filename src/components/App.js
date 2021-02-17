30c5c03c9096fce681a18e5cea52295c5dbdad31
Jason Noxon <jnoxon@caredesignny.org>
2021-02-17 00:05:27 -0500

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand mb-0 h1" href="/">
+            Jason Noxon
+          </a>
+        </div>
+      </nav>
+      <div class="container">
+        <h2>So here's the deal...</h2>
+        <p>
+          I'm trying to get my personal site up, and was messing around with a
+          bunch of frameworks (Gatbsy, Jeckyl), and I was struggling because I
+          just didn't have the time. Therefore, I am building from scratch using
+          React.
+        </p>
+        <p>Wish me luck...</p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
