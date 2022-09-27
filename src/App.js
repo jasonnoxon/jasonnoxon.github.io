f98a704fa698543d4bd26596b005080d31fca98d
Jason Noxon <jnoxon@caredesignny.org>
2022-09-27 18:51:11 -0400

    Updated website, added images

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from 'react-bootstrap/Image'
+import Row from "react-bootstrap/Row";
+
+
 import './App.css';
+import jason from "./Images/jason_noxon_round.png"
+import MailChimp from "./MailChimp";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload, dudes!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row style={{ marginTop: "70px" }}>
+
+        <Col>
+          <Image className="center" style={{ border: "0" }} src={jason} />
+        </Col>
+
+      </Row>
+      <Row><h1 className="header text-center">Jason Noxon</h1></Row>
+      <Row style={{ marginTop: "70px" }}>
+        <Col>&nbsp;</Col>
+        <Col sm={6}>
+          <p>
+            <span className="bigger">Listen...</span>anyone can create a website with lots of flashy colors, animations, and images to tantilize and inspire. It takes a real <strong>genius</strong> to
+            create something stark and beautifully clean like the website you're looking at now. It also takes a real wack-job to try and and convince you that
+            any of the above statement is true.
+          </p>
+
+          <p>
+            Regardless, my album entitled <strong><i>Dwell</i></strong> will be released on <strong>January 15, 2023</strong>. I would <i>really</i> like to keep you up to date
+            with what is happening with it. So if you wouldn't mind typing your first name and email address into the boxes provided below, I can do that in earnest.
+          </p>
+          <p>
+            This is an adventure for which I would be greatful to have you along.
+          </p>
+        </Col>
+        <Col>&nbsp;</Col>
+      </Row>
+      <Row>
+        <Col>&nbsp;</Col>
+        <Col>
+          <MailChimp />
+        </Col>
+        <Col>&nbsp;</Col>
+      </Row>
+    </Container >
+
   );
 }
 
