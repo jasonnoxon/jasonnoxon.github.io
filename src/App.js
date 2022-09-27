449923947f0b542f450fd6877c4ae9e5e34d46e2
Jason Noxon <jnoxon@caredesignny.org>
2022-09-27 15:23:13 -0400

    testing if it auto deploys by pushing a change to main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload, dude!
+          Edit <code>src/App.js</code> and save to reload, dudes!
         </p>
         <a
           className="App-link"
